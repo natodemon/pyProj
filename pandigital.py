d29adf7fbc3629ef1738e7388949815ef5614f69
natodemon <aes17optimus@gmail.com>
2022-07-16 10:49:33 +0200

    Initial pandigital, create task 2

---
+++ pandigital.py
@@ -0,0 +1,35 @@
+def isPandigital(num):
+    num_digits = str(num)
+
+    num_list = []
+
+    for digit in num_digits:
+        if digit in num_list:
+            return False
+        else:
+            num_list.append(digit)
+
+    regular_pan = []
+    for x in range(1, len(num_digits)+1):
+        regular_pan.append(str(x))
+
+    if sorted(num_list) == regular_pan:
+        return True
+    else:
+        return False
+
+def numSetGen(max_val):
+    num_set = set()
+    num_set.add(1)
+    for i in range(2, max_val+1):
+        #print(i)
+        if isPandigital(i):
+            num_set.add(i)
+
+    return num_set
+
+
+val = input('Please enter D: ')
+
+pandigitals = numSetGen(int(val))
+print(len(pandigitals))
